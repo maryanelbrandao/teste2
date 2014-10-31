a9ee09f4bca6a88f03bab69fe2f452b4bbc6dc4b
Maryane Lorena <mlorena@thor.localdomain>
2014-10-30 21:28:44 -0400

    add prog2

---
+++ somadoisnumeros.java
@@ -0,0 +1,16 @@
+public class somadoisnumeros{
+	
+
+	public static void main(String args[]){
+
+
+	int num1=4;
+	int num2=3;
+	int soma=0;
+	
+soma= num1+num2;
+
+System.out.println("Meu valor eh" + soma);
+	}
+}
+
